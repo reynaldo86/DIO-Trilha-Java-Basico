91814bc5040f8857ca2f3fe2283595b5ba8f6dea
Hendson <80369346+reynaldo86@users.noreply.github.com>
2023-03-01 19:54:46 -0300

    estruturas de repeticao

---
+++ Estruturas-de-Repeticao/src/exercicios/ExercicioWhile.java
@@ -0,0 +1,28 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class ExercicioWhile {
+    /*
+    🔸Nome e Idade: Faça um programa que leia conjuntos de dois valores,
+    o primeiro representando o nome do aluno e o segundo representando a sua idade.
+    (Pare o programa inserindo o valor 0 no campo nome)
+    */
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+
+            System.out.println("Digite seu nome: ");
+            String nome = input.next();
+
+            if(nome.equals("0")) break;
+
+            System.out.println("Qual a sua idade: ");
+            int idade = input.nextInt();
+        }
+    }
+    }
+
---
+++ Estruturas-de-Repeticao/src/exercicios/ExercicioWhileNota.java
@@ -0,0 +1,28 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class ExercicioWhileNota {
+    /*
+    🔸Nota: Faça um programa que peça uma nota, entre zero e dez.
+    Mostre uma mensagem caso o valor seja inválido e continue pedindo
+    até que o usuário informe um valor válido.
+    */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        while(true) {
+            System.out.println("Digite uma valor entre 0 e 10: ");
+            int numero = input.nextInt();
+
+            if(numero >= 0 && numero <= 10) {
+                break;
+
+            }else {
+                System.out.println("Valor inválido.");
+            }
+        }
+        System.out.println("Programa finalizado.");
+
+    }
+}
