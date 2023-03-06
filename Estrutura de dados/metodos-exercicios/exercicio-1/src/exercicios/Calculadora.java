67ca70f206ea22e508a5cb9b0ce9d98bda6e108e
Hendson <80369346+reynaldo86@users.noreply.github.com>
2023-03-06 19:41:55 -0300

    exercicios metodos

--- Estrutura de dados/metodos-exercicios/exercicio-1/src/exercicios/Calculadora.java
+++ Estrutura de dados/metodos-exercicios/exercicio-1/src/exercicios/Calculadora.java
@@ -5,18 +5,18 @@ Calcula as 4 operações básicas: soma, subtração, multiplicação e divisão
 Sempre 2 valores devem ser passados.
 
  */
-public class OperacoesMatematicas {
+public class Calculadora {
 
-    private static void somar(int valor1, int valor2) {
+    public static void somar(int valor1, int valor2) {
         System.out.println(valor1 + valor2);
     }
-    private static void subtrair(int valor1, int valor2) {
+    public static void subtrair(int valor1, int valor2) {
         System.out.println(valor1 - valor2);
     }
-    private static void multiplicar(int valor1, int valor2) {
+    public static void multiplicar(int valor1, int valor2) {
         System.out.println(valor1 * valor2);
     }
-    private static void dividir(Double valor1, Double valor2) {
+    public static void dividir(Double valor1, Double valor2) {
         if(valor1 == 0 || valor2 == 0){
             System.out.println("Nenhum valor pode ser divididos por zero");
         }else {
@@ -25,14 +25,7 @@ public class OperacoesMatematicas {
 
     }
 
-    public static void main(String[] args) {
 
-        somar(456,544);
-        subtrair(456,544);
-        multiplicar(456,544);
-        dividir(456d,544d);
-
-    }
 
 
 }
--- Estrutura de dados/metodos-exercicios/exercicio-1/src/exercicios/Mensagem.java
+++ Estrutura de dados/metodos-exercicios/exercicio-1/src/exercicios/Mensagem.java
@@ -3,19 +3,21 @@ package exercicios;
 A partir da hora do dia, informe a mensagem de bom dia, boa tarde e boa noite.
  */
 
-public class Horario {
+public class Mensagem {
 
     public static void horarios(int horas){
-        if(horas >= 0 || horas < 12){
+        if(horas >= 0 && horas < 12){
             System.out.println("Bom dia!!!");
-        } else if (horas >= 12 || horas < 18) {
+        }
+        else if (horas >= 12 && horas < 18) {
             System.out.println("Boa tarde!!!");
-        } else if (horas >=18 || horas < 23){
+        }
+        else if (horas >=18 && horas < 23){
             System.out.println("Boa noite!!!");
+        } else if (horas < 0 || horas > 23) {
+            System.out.println("Valor inválido, digite uma hora entre 0 e 23.");
         }
 
     }
-    public static void main(String[] args) {
-        horarios(5);
-    }
+
 }
