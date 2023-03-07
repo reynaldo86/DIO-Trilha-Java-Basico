c0160bb6aad297992e8c1447ed0f846ff602d7d2
Hendson <80369346+reynaldo86@users.noreply.github.com>
2023-03-07 14:22:05 -0300

    exercicios metodos sobrecarga com return.

--- Estrutura de dados/metodos-exercicios/exercicio-1/src/Main.java
+++ Estrutura de dados/metodos-exercicios/exercicio-1/src/Main.java
@@ -1,7 +1,4 @@
-import exercicios.Calculadora;
-import exercicios.Emprestimo;
-import exercicios.Mensagem;
-import exercicios.Sobrecarga;
+import exercicios.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -29,5 +26,10 @@ public class Main {
         Sobrecarga.calcularArea(6);
         Sobrecarga.calcularArea(5,8);
         Sobrecarga.calcularArea(5,8,9);
+
+        //Calcular area com return.
+        System.out.println(CalculoArea.calcularQuadrado(4)+ "m²");
+        System.out.println(CalculoArea.calcularRetangulo(4,8)+ "m²");
+        System.out.println(CalculoArea.calcularTrapezio(4.5,5.8,9.0) + "m²");
     }
 }
